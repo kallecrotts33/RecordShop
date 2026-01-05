b4d0a3837b81243efb6fbde97ee81aac402e5f14
kallecrotts33 <karcro@edu.novia.fi>
2026-01-05 10:37:25 +0200

    Spoofed "checkAdmin()"

--- webshop/middleware/checkIsAdmin.js
+++ webshop/middleware/checkIsAdmin.js
@@ -1,7 +1,11 @@
+// function checkIsAdmin(req, res, next) {
+//     if (req.user && req.user.role === 'admin') {
+//         return next();
+//     }
+// }
+
 function checkIsAdmin(req, res, next) {
-    if (req.user && req.user.role === 'admin') {
-        return next();
-    }
+    return next();
 }
 
 module.exports = checkIsAdmin;
